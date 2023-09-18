b97f0f017802353b203b5ec40306c5c82c187b47
singlerr <singlerr@naver.com>
2023-09-18 15:03:24 +0900

    init

---
+++ handler/src/lib.rs
@@ -0,0 +1,6 @@
+#![no_std]
+
+mod panichandler;
+
+
+
---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#![no_std]
+#![no_main]
+
+use handler;
+
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {
+        
+    }
+}
+
+
